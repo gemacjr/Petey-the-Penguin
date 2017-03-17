b6183c4cd7117f66fdb5e38cf9c5a9a8abff407c
Ed <edmccormic@gmail.com>
2017-03-17 10:56:12 -0600

    added files for enemies and environment

---
+++ Petey the Penguin/Bat.swift
@@ -0,0 +1,41 @@
+//
+//  Bat.swift
+//  Petey the Penguin
+//
+//  Created by Ed McCormic on 3/17/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import SpriteKit
+
+class Bat: SKSpriteNode, GameSprite {
+    var initialSize = CGSize(width: 44, height: 24)
+    var textureAtlas:SKTextureAtlas =
+        SKTextureAtlas(named: "Enemies")
+    var flyAnimation = SKAction()
+    
+    init() {
+        super.init(texture: nil, color: .clear,
+                   size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius:
+            size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+        createAnimations()
+        self.run(flyAnimation)
+    }
+    
+    func createAnimations() {
+        let flyFrames:[SKTexture] = [
+            textureAtlas.textureNamed("bat"),
+            textureAtlas.textureNamed("bat-fly")
+        ]
+        let flyAction = SKAction.animate(with: flyFrames,
+                                         timePerFrame: 0.12)
+        flyAnimation = SKAction.repeatForever(flyAction)
+    }
+    
+    func onTap() {}
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    } 
+} 
---
+++ Petey the Penguin/MadFly.swift
@@ -0,0 +1,42 @@
+//
+//  MadFly.swift
+//  Petey the Penguin
+//
+//  Created by Ed McCormic on 3/17/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import SpriteKit
+
+class MadFly: SKSpriteNode, GameSprite {
+    var initialSize = CGSize(width: 61, height: 29)
+    var textureAtlas:SKTextureAtlas =
+        SKTextureAtlas(named: "Enemies")
+    var flyAnimation = SKAction()
+    
+    init() {
+        super.init(texture: nil, color: .clear,
+                   size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius:
+            size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+        createAnimations()
+        self.run(flyAnimation)
+    }
+    
+    func createAnimations() {
+        let flyFrames:[SKTexture] = [
+            textureAtlas.textureNamed("madfly"),
+            textureAtlas.textureNamed("madfly-fly")
+        ]
+        
+        let flyAction = SKAction.animate(with: flyFrames,
+                                         timePerFrame: 0.14)
+        flyAnimation = SKAction.repeatForever(flyAction)
+    }
+    
+    func onTap() {}
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder) 
+    } 
+} 
